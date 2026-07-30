b47cc7fe2665fd55fa20f5697cf899d46ee5803b
paimand <emmengibar@msn.com>
2026-07-30 12:47:50 +0200

    server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware para ficheros estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Carga de Rutas Modularizadas
 const mergeRoute = require('./src/routes/mergeRoute');
